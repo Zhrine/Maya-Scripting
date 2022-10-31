845945da35cb5dea90977ef1a16bb56ec4283946
Zhrine <10787343@uvu.edu>
2022-10-30 18:18:49 -0600

    Crap Snome

--- Python/Build/My Scripts/CrapSnome.py
+++ Python/Build/My Scripts/CrapSnome.py
@@ -10,3 +10,4 @@ cmds.xform(snoBe1, r=True, os=True, t =(0,(snoSize[0]+snoSpace[1]) ,0))
 
 snoBe1 = cmds.polySphere( sx=15, sy=15, r=snoSize[2])
 cmds.xform(snoBe1, r=True, os=True, t =(0,(snoSize[0]+snoSize[1]+snoSpace[2] +1) ,0))
+#hi

0c4642cfb2b0b8c81fefe37cc096d5808c0fa57e
Zhrine <10787343@uvu.edu>
2022-12-09 21:35:06 -0700

    k

--- Python/Build/My Scripts/Sequential Renamer.py
+++ Python/Build/My Scripts/Sequential Renamer.py
@@ -1,1 +1,16 @@
 import maya.cmds as cmds
+#in class notes
+txt = "leg_###_jot"
+
+i = 1
+pre = 'leg_'
+suffix = '_jot'
+x = str(i).[fll](4)
+
+print(x)
+
+txt = "leg_###_jnt_####"
+
+count = txt.count("#")
+if > 0:
+    x = txt.find("#" * count)

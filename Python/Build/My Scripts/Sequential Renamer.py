9261148d8a46125ca84f863f2b8e227988a0f9c9
Zhrine <10787343@uvu.edu>
2022-12-14 18:07:31 -0700

    Got One finally working!
    All with the help of other classmates, i really gotta learn more coding knowledge from the internet

--- Python/Build/My Scripts/Sequential Renamer.py
+++ Python/Build/My Scripts/Sequential Renamer.py
@@ -1,16 +1,15 @@
 import maya.cmds as cmds
-#in class notes
-txt = "leg_###_jot"
 
-i = 1
-pre = 'leg_'
-suffix = '_jot'
-x = str(i).[fll](4)
+list = ["Leg_##_Jnt","Leg_##_Jnt","Leg_##_Jnt","Leg_##_Jnt","Leg_##_Jnt","Leg_##_Jnt","Leg_##_Jnt","Leg_##_Jnt","Leg_##_Jnt"]
+zeros = 0
+argu_num = 1
+hashtag = "#"
 
-print(x)
-
-txt = "leg_###_jnt_####"
-
-count = txt.count("#")
-if x > 0:
-    x = txt.find("#" * count)
+for argument in list:
+   for hashtag in argument:
+      zeros += 1
+   new_argu = argument.replace("#", "0", zeros)
+   new_argu = new_argu.replace("0","", len(str(argu_num)))
+   new_under = new_argu.find("_", new_argu.find("_")+1)
+   print(new_argu[:new_under]+ str(argu_num) +new_argu[new_under:])
+   argu_num += 1
